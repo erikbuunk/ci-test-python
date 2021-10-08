0034ed8e15403716a8ae83f729418156bccefbb2
Erik Buunk <erik.buunk@gmail.com>
2021-10-08 16:07:18 +0200

    print curdir

--- 02_scripts/settings.py
+++ 02_scripts/settings.py
@@ -4,5 +4,6 @@ import json
 
 """Load Config File"""
 cur_dir = os.path.dirname(os.path.realpath(__file__))
+print(cur_dir)
 with open(os.path.join(cur_dir, "config.json")) as config_file:
     config = json.load(config_file)
